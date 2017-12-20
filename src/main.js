116580c3d6a3f9bc1c11fb898f9e95a1ac35e687
Raegan Millhollin <desdemonhu@gmail.com>
2017-12-20 07:57:35 -0800

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import './style/main.scss';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    ///state goes here
+  }
+
+  render(){
+    return ///templated hmtl
+  }
+}
