338e91e230596a57a71eb50f8a8dc6f7e90f335b
Raegan Millhollin <desdemonhu@gmail.com>
2017-12-25 17:41:38 -0800

    Fixed the form-category file. it got horked up

--- src/component/form-category/index.js
+++ src/component/form-category/index.js
@@ -2,36 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Category from '../category'
 
-let button;
-
-class ExpenseForm extends React.Component {
+class FormCategory extends React.Component {
   constructor(props){
     super(props)
     ///state goes here
     this.state = {
-      view: this.props.showExpenseUpdate,
       local: {
         name: '',
-        price: 0,
-      },
+        budget: 0,
+      }
     }
-  }
-
-  updateForm = () => {
-    this.setState({local: this.props.expense,})
-    button = (
-      <div>
-        <button value={this.props.expense.key} onClick={this.expenseUpdate}>Update</button>
-        <button onClick={this.props.hideExpenseUpdate}>Cancel</button>
-      </div>
-    )
   }
 
   clearState(){
     this.state.local = {
       name: '',
-      price: 0,
-      category: '',
+      budget: 0,
     }
   }
 
@@ -40,53 +26,16 @@ class ExpenseForm extends React.Component {
                           name: event.target.value}})
   }
 
-  priceUpdate = (event) => {
+  budgetUpdate = (event) => {
     this.setState({local: {...this.state.local,
-                          price: event.target.value}})
+                          budget: event.target.value}})
   }
 
-  categoryUpdate = (event) => {
-    this.setState({local: {...this.state.local,
-                          category: event.target.value}})
-  }
-
-  createExpense = (e) => {
+  createCateogry = (e) => {
     e.preventDefault();
     this.props.onComplete(this.state.local);
     this.clearState();
   }
-
-  expenseUpdate = (e) => {
-    e.preventDefault()
-    this.props.expenseUpdate({key: e.target.value,
-                            update: this.state.local,})
-    this.props.hideExpenseUpdate();
-    this.clearState();
-  }
-
-componentWillMount(){
-  if(this.props.expense && this.state.view === true){
-    this.updateForm();
-    }else {
-      button = (
-        <button type="submit" onClick={this.createExpense}>Add Expense</button>)
-    }
-}
-
-componentDidUpdate(){
-  if(this.state.local.name === ''){
-    button = button = (
-      <button type="submit" onClick={this.createExpense}>Add Expense</button>)
-    }
-}
-
-// shouldComponentUpdate(nextProps, nextState){
-//   if(nextProps.categories){
-//     return true
-//   }else {
-//     return false
-//   }
-// }
 
   componentWillReceiveProps(nextProps){
     if(nextProps.state){
@@ -97,22 +46,15 @@ componentDidUpdate(){
   render(){
     return (
       <div>
-        <h1>Expense Form</h1>
+        <h1>Form Category</h1>
         <form>
-        <select name="category-select" onFocus={this.categoryUpdate}
-        onChange={this.categoryUpdate}>
-        {this.props.categories.map((category, i) => {
-            return <option key={i}
-            value={category.key}>{category.name}</option>
-        })}
-        </select>
           <input type="text" value={this.state.local.name} onChange={this.nameUpdate}></input>
-          <input type="number" value={this.state.local.price} onChange={this.priceUpdate}></input>
-          {button}
+          <input type="number" value={this.state.local.budget} onChange={this.budgetUpdate}></input>
+          <button type="submit" onClick={this.createCateogry}>Add Category</button>
         </form>
       </div>
       )
   }
 }
 
-export default ExpenseForm
+export default FormCategory
